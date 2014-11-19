e33003f72257b0eaa82e906114fd3cad9422f9d4
Swan Chase <16647@ma-web.nl>
2014-11-19 14:42:57 +0100

    Delay Script
    for if something needs delaying

---
+++ horror-game/Assets/Scripts/DelayScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayScript : MonoBehaviour {
+	
+
+	void Start () {
+		Invoke("DelayInvoke",(Random.Range(1, 5)));
+	}
+
+	void TimerInvoke()
+	{
+		Debug.Log("ping");
+	}
+
+}
